8ac51f31fb42178387f77a9df099b482704c3431
hamid_ <hmdmlkv@gmail.com>
2015-03-08 16:36:54 +0330

    first hamid commit

--- gen/ir/eynakgroup/derbi/R.java
+++ gen/ir/eynakgroup/derbi/R.java
@@ -9,7 +9,7 @@ package ir.eynakgroup.derbi;
 
 public final class R {
     public static final class array {
-        public static final int positions=0x7f080000;
+        public static final int positions=0x7f070000;
     }
     public static final class attr {
         /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
@@ -111,7 +111,7 @@ containing a value of this type.
         public static final int pstsUnderlineHeight=0x7f010004;
     }
     public static final class color {
-        public static final int background_tab_pressed=0x7f060000;
+        public static final int background_tab_pressed=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -121,8 +121,8 @@ containing a value of this type.
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f070000;
-        public static final int activity_vertical_margin=0x7f070001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int background_tab=0x7f020000;
@@ -170,34 +170,14 @@ containing a value of this type.
         public static final int opleader_board=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int title_activity_opleader_board=0x7f040001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int title_activity_opleader_board=0x7f060001;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
 
 
@@ -222,14 +202,12 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
-        public static final int FullscreenTheme=0x7f050002;
+        public static final int AppTheme=0x7f080001;
+        public static final int FullscreenTheme=0x7f080002;
     }
     public static final class styleable {
         /** Attributes that can be used with a PagerSlidingTabStrip.
